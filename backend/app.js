e6735ddb415cff6d6e900c40d41c2c9cd087f846
Yash <anantyash.2710@gmail.com>
2026-07-06 00:13:31 +0530

    UI Update

--- backend/app.js
+++ backend/app.js
@@ -5,20 +5,20 @@ import { hitesh } from "./personas/hc.js";
 import { piyush } from "./personas/pg.js";
 
 // For frontend
-// import path from "path";
-// import { fileURLToPath } from "url";
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // frontend ---------------
-// app.use(express.static(path.join(__dirname, "../")));
+app.use(express.static(path.join(__dirname, "../")));
 
-// app.get("/", (req, res) => {
-//   res.send("Home route is working");
-// });
+app.get("/", (req, res) => {
+  res.send("Home route is working");
+});
 // ------------------------
 
 app.use(cors());
